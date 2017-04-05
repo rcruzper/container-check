d963f68e60209c0d03254b702c1289e6ecbf4eaa
Raúl Cruz <rcruzper@gmail.com>
2017-04-05 16:49:02 +0200

    add timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-until [[ $(curl -sL -w %{http_code}\\n $1 -o /dev/null) == 200 ]]
+until [[ $(curl -sL -m 2 -w %{http_code}\\n $1 -o /dev/null) == 200 ]]
 do
     echo Waiting service
     sleep 2
